5345feba2e28a31c38f54f9cd9f2d8f5cb71df41
Ahmed Abdulazeem <ahmadazeem40@gmail.com>
2023-09-09 02:15:10 +0300

    Add print numberz c file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - write the base 10 numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+	putchar(n);
+	n++;
+	}
+	putchar('\n');
+	return (0);
+}
