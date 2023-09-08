c35959c70fbee5591d017a1a146b42a481dabd6c
Ahmed Abdulazeem <ahmadazeem40@gmail.com>
2023-09-09 02:11:27 +0300

    Add print numbers c file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - write the base 10 numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+	printf("%d", n);
+	}
+	putchar('\n');
+	return (0);
+}
+
