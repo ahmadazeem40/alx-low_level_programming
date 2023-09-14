7db88aa8f632c8112f61f6b34e0bd6c929084062
Ahmed Abdulazeem <ahmadazeem40@gmail.com>
2023-09-14 19:58:06 +0300

    Hot fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * print_numbers - print numbers from 0 to 9
+ * print_most_numbers - print numbers from 0 to 9
  * Return: void
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i = 48;
 
 	while (i >= 48 && i <= 57)
 	{
+		if(i != 50 && i != 52)
 		_putchar(i);
 		i++;
 	}
