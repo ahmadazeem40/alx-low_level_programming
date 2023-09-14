de484ba7701e246071954d7f7656e087b421d840
Ahmed Abdulazeem <ahmadazeem40@gmail.com>
2023-09-14 18:56:09 +0300

    Add isdigit file

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if a letter is upper case or not
+ * @c: char to be checked
+ * Return: 1 if upper 0 if not
+ */
+
+int _isupper(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
+
