369c49e04dbb4a20586bed07cdd5a1da7d10a999
Ahmed Abdulazeem <ahmadazeem40@gmail.com>
2023-09-18 21:01:59 +0300

    Add print reverse file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+void print_rev(char *s);
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse
+ * @s: string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
