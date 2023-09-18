6c93885bb7901708412c425874b8231605b169ea
Ahmed Abdulazeem <ahmadazeem40@gmail.com>
2023-09-18 23:29:59 +0300

    Hot fix

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,11 +14,11 @@ void print_array(int *a, int n)
 	{
 		if (i == n - 1)
 		{
-			_putchar(a[i] + 48);
+			_putchar(a[i]);
 		}
 		else
 		{
-			_putchar(a[i] + 48);
+			_putchar(a[i]);
 			_putchar(',');
 			_putchar(' ');
 		}
