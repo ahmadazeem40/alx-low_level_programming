d5a7da5899ba270bedef0fe53f32bbc0a43975e3
Ahmed Abdulazeem <ahmadazeem40@gmail.com>
2023-09-18 23:29:04 +0300

    Add print array file

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_array - print a number of array elements
+ * @a: array
+ * @n: num of elements to print
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == n - 1)
+		{
+			_putchar(a[i] + 48);
+		}
+		else
+		{
+			_putchar(a[i] + 48);
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+}
