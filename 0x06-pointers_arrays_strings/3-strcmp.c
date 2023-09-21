a14f593f521a7606a5e39b2f0bd9af72a533f7eb
Ahmed Abdulazeem <ahmadazeem40@gmail.com>
2023-09-22 00:58:51 +0300

    Hot fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,15 +8,17 @@
   */
 int _strcmp(char *s1, char *s2)
 {
-	int i, r = 0;
+	int r = 0;
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	while (*s1)
 	{
-		if (s1[i] != s2[i])
+		if (*s1 != *s2)
 		{
 			r = ((int)*s1 - 48) - ((int)*s2 - 48);
 			break;
 		}
+		s1++;
+		s2++;
 	}
 	return (r);
 }
