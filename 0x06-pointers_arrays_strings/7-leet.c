ff58c704a989f37e5b6a4b1bb43390e32ec3c48c
Ahmed Abdulazeem <ahmadazeem40@gmail.com>
2023-09-22 02:35:42 +0300

    Hot fix

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,7 +9,7 @@
 char *leet(char *c)
 {
 	char *cp = c;
-	char key[] = {'A', 'E', 'O', 'T', 'L');
+	char key[] = {'A', 'E', 'O', 'T', 'L'};
 	int value[] = {4, 3, 0, 7, 1};
 	unsigned int i;
 
@@ -17,7 +17,7 @@ char *leet(char *c)
 	{
 		for (i = 0; i < sizeof(key) / sizeof(char); i++)
 		{
-			if (*c == key[i] || *c key[i] + 32)
+			if (*c == key[i] || *c == key[i] + 32)
 			{
 				*c = 48 + value[i];
 			}
