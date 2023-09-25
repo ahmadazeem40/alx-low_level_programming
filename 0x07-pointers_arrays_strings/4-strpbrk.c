39775941b3215731877457afb1c398e77d5103fd
Ahmed Abdulazeem <ahmadazeem40@gmail.com>
2023-09-26 01:44:19 +0300

    Hot fix

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,15 +9,19 @@
 char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i, j;
+	char *p;
 
 	for (i = 0; s[i] >= '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (accept[j] == s[i])
-				return (&s[i]);
+			{
+				p = &s[i];
+				return (p);
+			}
 		}
 	}
-	return ('\0');
+	return (0);
 }
 
