e07c3d63c4b057fb21f426c6479bdb70c53acd79
Ahmed Abdulazeem <ahmadazeem40@gmail.com>
2023-10-03 00:35:08 +0300

    hot fix

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,5 +1,19 @@
+#include "main.h"
+
 int _atoi(char *s)
 {
-	return (0);
+	unsigned int num = 0;
+	int sign = 1;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
+
+	return (num * sign);
 }
 
