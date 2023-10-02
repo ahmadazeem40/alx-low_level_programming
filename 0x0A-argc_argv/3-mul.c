dad8a770f84c8ab20ab0ae7bfb0fb594dd6c05c8
Ahmed Abdulazeem <ahmadazeem40@gmail.com>
2023-10-03 01:35:01 +0300

    Add mul file

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - print sum of 2 nums
+  * @argc: number
+  * @argv: list
+  * Return: 0
+  */
+
+int main(int argc, char **argv)
+{
+	int sum;
+
+	if (argc == 3)
+	{
+		sum = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
+
