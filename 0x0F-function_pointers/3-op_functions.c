5f49573db22bfecd18d89978d024cbf00866325c
Ahmed Abdulazeem <ahmadazeem40@gmail.com>
2023-10-12 05:17:52 +0300

    Add last files

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include "3-calc.h"
+
+/**
+  * op_add - add numbers
+  * @a: first int
+  * @b: second int
+  * Return: result
+  */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+  * op_sub - sub numbers
+  * @a: first int
+  * @b: second int
+  * Return: result
+  */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+  * op_mul - mul numbers
+  * @a: first int
+  * @b: second int
+  * Return: result
+  */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+  * op_div - div numbers
+  * @a: first int
+  * @b: second int
+  * Return: result
+  */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+  * op_mod - mod numbers
+  * @a: first int
+  * @b: second int
+  * Return: result
+  */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
