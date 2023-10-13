9a2525ffbfff287ba1b5f100c755595de1b024c4
Ahmed Abdulazeem <ahmadazeem40@gmail.com>
2023-10-13 03:37:00 +0300

    Cold fix

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,14 +15,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ap;
 	unsigned int i;
 
+	if(!n)
+	{
+		printf("\n");
+		return;
+	}
+
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(ap, int));
-		if (i != n - 1 && separator)
-			printf(separator);
+		printf("%d%s", va_arg(ap, int), i < n - 1 ? (separator ? separator : "") : "\n");
 	}
-	_putchar('\n');
 
 	va_end(ap);
 }
