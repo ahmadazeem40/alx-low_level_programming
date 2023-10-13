e31de2000860d24d2d5d88f343133d2180506bf6
Ahmed Abdulazeem <ahmadazeem40@gmail.com>
2023-10-13 03:02:28 +0300

    Add sum them all file

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+  * sum_them_all - sum all params
+  * @n: num of params
+  * Return: sum
+  */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	int i, sum = 0;
+
+	if (n == 0)
+		return (0);
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+	return (sum);
+}
+
