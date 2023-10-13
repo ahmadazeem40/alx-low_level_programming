3b867c25133bea4d4fe6f92b016efb5a17f14ada
Ahmed Abdulazeem <ahmadazeem40@gmail.com>
2023-10-13 04:22:52 +0300

    Hot fix

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,7 +5,7 @@ typedef struct token
 {
 	char *token;
 	void (*f)(char *, va_list);
-} toket_t;
+} token_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
