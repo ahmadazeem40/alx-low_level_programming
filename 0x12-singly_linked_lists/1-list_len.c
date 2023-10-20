66b763ebaece407d7defbbf4d348c3f4b7c9587f
Ahmed Abdulazeem <ahmadazeem40@gmail.com>
2023-10-20 03:51:53 +0300

    Add list len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+  * list_len - return length of linked list
+  * @h: linked list
+  * Return: length
+  */
+
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+		i++;
+	return (i);
+}
+
