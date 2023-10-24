8eae078921719a692de8fa2397523ec22ca110f9
Ahmed Abdulazeem <ahmadazeem40@gmail.com>
2023-10-25 01:31:06 +0300

    Add free list int

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - free list
+ * @head: first node
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *node;
+
+	while (head)
+	{
+		node = head;
+		head = head->next;
+		free(node);
+	}
+}
