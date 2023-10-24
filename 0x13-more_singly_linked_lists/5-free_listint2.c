e4193149e80982fb4598f613daea217f89fab10d
Ahmed Abdulazeem <ahmadazeem40@gmail.com>
2023-10-25 01:39:50 +0300

    Hot fix

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,9 +5,9 @@
  * @head: first node
  * Return: void
  */
-void free_listint2(listint_t *head)
+void free_listint2(listint_t **head)
 {
-	listint_t *node, node2;
+	listint_t *node, *node2;
 
 	if (!head)
 		return;
